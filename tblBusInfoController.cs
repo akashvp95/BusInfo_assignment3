f371c46d9a5b088c5d80e6bb740e06a717c05e54
akashvp95 <98869796+akashvp95@users.noreply.github.com>
2022-05-01 12:28:40 +0530

    Add files via upload

---
+++ tblBusInfoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusInfo_Assignment3.Models;
+
+namespace BusInfo_Assignment3.Controllers
+{
+    public class tblBusInfoController : Controller
+    {
+
+        BusInfo_assignmentEntities db = new BusInfo_assignmentEntities();
+        public ActionResult Index()
+        {
+            return View((from c in db.tblBusInfoes
+                        where c.BoardPoint == "MUM"
+                        select c));
+        }
+    }
+}
